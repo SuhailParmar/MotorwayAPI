c3cbd51552f0ad5dc7730f16f260837a2653570f
Suhail Parmar <suhailparmar@zenyatta.lan>
2018-11-04 12:40:58 +0000

    Use Postgres backend

--- motorway_project/motorway_project/settings.py
+++ motorway_project/motorway_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'motorway_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'FAIL'),
+        'USER': os.environ.get('POSTGRES_USER', 'FAIL'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'FAIL'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
