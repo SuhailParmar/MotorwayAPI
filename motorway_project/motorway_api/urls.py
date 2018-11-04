f05d5c2e2b8cb62c1c6f75a104fc7cab10ddeca5
Suhail Parmar <suhailparmar@zenyatta.lan>
2018-11-04 15:51:43 +0000

    Appended urls for /event/id and /events/all

--- motorway_project/motorway_api/urls.py
+++ motorway_project/motorway_api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import ListAllEventsView
 from .views import CreateEventView
+from.views import RetrieveDestroyEventView
 
 
 urlpatterns = [
     path('', CreateEventView.as_view(), name="create"),  # Post to root url
-    path('all/', ListAllEventsView.as_view(), name="events-all")
+    path('all/', ListAllEventsView.as_view(), name="events-all"),
+    path('<int:pk>', RetrieveDestroyEventView.as_view(), name='events-retrievedestroy')
 ]
