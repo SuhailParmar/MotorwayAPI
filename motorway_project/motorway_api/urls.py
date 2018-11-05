2f429a70960b4ec1818c93c8b0296f6ae1db95e6
Suhail Parmar <suhailparmar@zenyatta.lan>
2018-11-05 11:58:39 +0000

    Using New CreateFIlter view mapped to /events

--- motorway_project/motorway_api/urls.py
+++ motorway_project/motorway_api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import ListAllEventsView
-from .views import CreateEventView
+from .views import CreateFilterView
 from.views import RetrieveDestroyEventView
 
 
 urlpatterns = [
-    path('', CreateEventView.as_view(), name="create"),  # Post to root url
+    path('', CreateFilterView.as_view(), name="create-filter"),  # Post to root url
     path('all/', ListAllEventsView.as_view(), name="events-all"),
     path('<int:pk>', RetrieveDestroyEventView.as_view(), name='events-retrievedestroy')
 ]
--- motorway_project/motorway_api/views.py
+++ motorway_project/motorway_api/views.py
@@ -1,9 +1,9 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.generics import CreateAPIView
 from rest_framework.generics import RetrieveDestroyAPIView
+from rest_framework.generics import ListCreateAPIView
 from .models import MotorwayEvent
 from .serializers import MotorwayEventSerializer
-
 
 """
 Views are how to get objects from the database
@@ -12,24 +12,22 @@ https://www.django-rest-framework.org/api-guide/generic-views/#generic-views
 
 
 class RetrieveDestroyEventView(RetrieveDestroyAPIView):
-    """
-    Get and DELETE from id /events/pk
-    """
+    """ Get and DELETE from id /events/pk """
     queryset = MotorwayEvent.objects.all()
     serializer_class = MotorwayEventSerializer
+
+
+class CreateFilterView(ListCreateAPIView):
+    """ POST (create) and GET (filtered) events at /events"""
+    serializer_class = MotorwayEventSerializer
+
+    def get_queryset(self):
+        query_params = self.request.query_params
+        # Need to unpack query_params as multiple kwargs: id: 0, info: hello
+        return MotorwayEvent.objects.filter(**query_params)
 
 
 class ListAllEventsView(ListAPIView):
-    """
-    Return a list of all the MotorwayEvents mapped to /all
-    """
-    queryset = MotorwayEvent.objects.all()
-    serializer_class = MotorwayEventSerializer
-
-
-class CreateEventView(CreateAPIView):
-    """
-    Ability to POST n MotorwayEvents at /events
-    """
+    """ Return a list of all the MotorwayEvents mapped to /all """
     queryset = MotorwayEvent.objects.all()
     serializer_class = MotorwayEventSerializer
