f43bd0be30a80cb9bc7e88cb7db926f49184e5ec
zarya <suhail.parmar@gmail.com>
2018-11-09 11:37:53 +0000

    post-events

--- motorway_project/motorway_tests/scripts/post_api_events.bash
+++ motorway_project/motorway_tests/scripts/post_api_events.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 curl -X POST -i -H "Content-Type: application/json"\
- --data '{"event_id":101,"junction":[1,2],"closest_cities":["test"],"extra_information":"test","motorway":6,"direction":"n","metadata":"test","reason":"test","time_timestamp":"2018-11-06T17:37:06","time_day_worded":"Mon","time_year":2018,"time_day_numerical":1,"time_hour":1,"time_minutes":1,"time_seconds":1}'\
+ --data '{"event_id":105,"junction":[1,2],"closest_cities":["test"],"extra_information":"test","motorway":6,"direction":"n","metadata":"test","reason":"test","time_timestamp":"2018-11-06T17:37:06","time_day_worded":"Mon","time_year":2018,"time_day_numerical":1,"time_hour":1,"time_minutes":1,"time_seconds":1}'\
     http://localhost:8000/api/events/
 
